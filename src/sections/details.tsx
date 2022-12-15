f63e3b76f53b2c6dce601096530f1744e902145e
Habeeb Kareem <olaidekareem43@yahoo.com>
2022-12-15 18:32:48 +0100

    Complete Details component.

--- src/sections/details.tsx
+++ src/sections/details.tsx
@@ -1,13 +1,70 @@
-import {VStack} from '@chakra-ui/react'
+import {
+  VStack,
+  Heading,
+  Text,
+  SimpleGrid,
+  GridItem,
+  FormControl,
+  FormLabel,
+  Input,
+  Select,
+  Checkbox,
+  Button,
+} from '@chakra-ui/react'
 
 const Details = () => {
   return (
-    <VStack
-      w="full"
-      h="full"
-      p={40}
-      spacing={10}
-      alignItems="flex-start"></VStack>
+    <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start">
+      <VStack spacing={3} alignItems="flex-start">
+        <Heading>Your details</Heading>
+        <Text>If you already have an account, click here to log in.</Text>
+      </VStack>
+      <SimpleGrid columns={2} columnGap={3} rowGap={6} w="full">
+        <GridItem colSpan={1}>
+          <FormControl>
+            <FormLabel>First Name</FormLabel>
+            <Input placeholder="John" />
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <FormControl>
+            <FormLabel>Last Name</FormLabel>
+            <Input placeholder="Doe" />
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <FormControl>
+            <FormLabel>Address</FormLabel>
+            <Input placeholder="Blv. Shattered Dreams 21" />
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <FormControl>
+            <FormLabel>City</FormLabel>
+            <Input placeholder="San Francisco" />
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <FormControl>
+            <FormLabel>Country</FormLabel>
+            <Select>
+              <option value="usa">United States of America</option>
+              <option value="uae">United Arab Emirates</option>
+              <option value="fr">France</option>
+              <option value="de">England</option>
+            </Select>
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <Checkbox defaultChecked>Ship to billing address.</Checkbox>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <Button size="lg" w="full">
+            Place Order
+          </Button>
+        </GridItem>
+      </SimpleGrid>
+    </VStack>
   )
 }
 
