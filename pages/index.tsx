fcf4c0f3648852fbd9efaf913c9e8ad0d616f8a7
Habeeb Kareem <olaidekareem43@yahoo.com>
2022-12-15 17:57:33 +0100

    Create cart.tsx and details.tsx in src/sections
    Created cart.tsx and details.tsx to hold separate components.
    
    Refactored index.tsx to use cart.tsx and details.tsx.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,12 @@
-import {Container, Flex, VStack} from '@chakra-ui/react'
+import {Container, Flex} from '@chakra-ui/react'
+import Cart from '../src/sections/cart'
+import Details from '../src/sections/details'
 
 const IndexPage = () => (
   <Container maxW="container.xl" p={0}>
     <Flex h="100vh" py={20}>
-      <VStack
-        w="full"
-        h="full"
-        p={40}
-        spacing={10}
-        alignItems="flex-start"></VStack>
-      <VStack
-        w="full"
-        h="full"
-        p={40}
-        spacing={10}
-        alignItems="flex-start"
-        bg="gray.50"></VStack>
+      <Details />
+      <Cart />
     </Flex>
   </Container>
 )
---
+++ src/sections/details.tsx
@@ -0,0 +1,14 @@
+import {VStack} from '@chakra-ui/react'
+
+const Details = () => {
+  return (
+    <VStack
+      w="full"
+      h="full"
+      p={40}
+      spacing={10}
+      alignItems="flex-start"></VStack>
+  )
+}
+
+export default Details
