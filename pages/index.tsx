e819616c6c1d62bf3dde1b3021b130f0c02fd226
Habeeb Kareem <olaidekareem43@yahoo.com>
2022-12-15 17:49:30 +0100

    Setup Layout
    Build layout using Container, Flex and VStack Component of Chakra UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,23 @@
-const IndexPage = () => <h1>Hello Next.js</h1>
+import {Container, Flex, VStack} from '@chakra-ui/react'
+
+const IndexPage = () => (
+  <Container maxW="container.xl" p={0}>
+    <Flex h="100vh" py={20}>
+      <VStack
+        w="full"
+        h="full"
+        p={40}
+        spacing={10}
+        alignItems="flex-start"></VStack>
+      <VStack
+        w="full"
+        h="full"
+        p={40}
+        spacing={10}
+        alignItems="flex-start"
+        bg="gray.50"></VStack>
+    </Flex>
+  </Container>
+)
 
 export default IndexPage
